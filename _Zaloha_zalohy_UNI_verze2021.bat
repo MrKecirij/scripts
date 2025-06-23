b84d5670fae5c03d53ac10d261c4e739ea6a2d5c
MrKecirij <140491141+MrKecirij@users.noreply.github.com>
2025-06-23 07:23:46 +0200

    dubna uprava o z5tne syncho pracovních adresářů MS3
    Update _Zaloha_zalohy_UNI_verze2021.bat z 2025-04-24_15-48. Po dvou výměnách to maká divně, bude se muset upravit, či anulovat tato část

--- _Zaloha_zalohy_UNI_verze2021.bat
+++ _Zaloha_zalohy_UNI_verze2021.bat
@@ -1,117 +1,138 @@
 echo off
 rem echo on
 echo cls
-echo nastaven  k˘dov  str nka
+echo nastavená kódová stránka
 chcp
-echo zapˇn m źeçtinu (pro jistotu)
+echo zapínám češtinu (pro jistotu)
 chcp 852
-echo ¬eçtina je zde: Řçźý§ě ˇ‚Ł…·ć¬ü¦íµÖéŢ
-echo Slovençtina je zde: – Ô ś • Ň › „ Ž ” ™  š
+echo Čeština je zde: ěščřžýáíéúůĚŠČŘŽÝÁÍÉÚŮ
+echo Slovenština je zde: ľ ď ť Ľ Ď Ť ä Ä ö Ö ü Ü
 echo *** 
-echo *** JIMovo UNI z loha z lohy Łźetnictvˇ ***
-echo zrcadlˇm z lohu Money S3 mezi origin lnˇm adres ýem,
-echo a z lohou na Google Disku.
+echo *** JIMovo UNI záloha zálohy účetnictví ***
+echo zrcadlím zálohu Money S3 mezi originálním adresářem,
+echo a zálohou na Google Disku.
 echo *** 
-echo Zrcadlˇm MS3 z lohu
+echo Zrcadlím MS3 zálohu
 echo ***
 
 rem napln promennou mnou nebo Petulkou, pocita ze to je Peta - default, prepis na me pokud jsem to ja.
 set kdoJsem=PEM
 if %USERNAME%==JIM set kdoJsem=JIM
 
-rem nastavenˇ zdrojov‚ho adres ýe - m me stejně
+rem nastavení zdrojového adresáře - máme stejný
 set primarniZdroj="C:\Users\Public\Documents\Solitea\Money S3\Data\Zalohy"
 
-rem diky zjiçtŘnˇ kdo jsem nastavˇm i cˇlově adres ý m…j nebo jejˇ
-rem zmŘna 24.6.2022, kdy si google usmyslel vyźlenit si vlastnˇ diskov‚ pˇsmeno pro sdˇlen‚ disky :-/ kret‚ni.. tak§e ka§dě to m me jinde...
+rem diky zjištění kdo jsem nastavím i cílový adresář můj nebo její
+rem změna 24.6.2022, kdy si google usmyslel vyčlenit si vlastní diskové písmeno pro sdílené disky :-/ kreténi.. takže každý to máme jinde...
 rem set primarniCilPEM="D:\DATA\GoogleDisk\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-%kdoJsem%"
 rem set primarniCilJIM="G:\.shortcut-targets-by-id\0B_CWm7QThRhveURlcUZmNm05M28\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-%kdoJsem%"
-rem zmŘna 22.3.2025,nynˇ je to statick‚, JIM je JIM a PEM je PEM, nepou§ˇvej zbyteźnŘ věpoźet, viz věçe
+rem změna 22.3.2025,nyní je to statické, JIM je JIM a PEM je PEM, nepoužívej zbytečně výpočet, viz výše
 set primarniCilPEM="D:\DATA\GoogleDisk\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-PEM"
 set primarniCilJIM="G:\.shortcut-targets-by-id\0B_CWm7QThRhveURlcUZmNm05M28\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-JIM"
+rem 2025-05-24 - doplnění synchro obsahu o miláčkův obsah - vše starší u mě mi dorovná
+set milackuvZdrojProPEM="D:\DATA\GoogleDisk\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-JIM"
+set milackuvZdrojProJIM="G:\.shortcut-targets-by-id\0B_CWm7QThRhveURlcUZmNm05M28\_JIMsBusinessPEM\05_provoz\02022605\eDOK\zalohy\MS3-PEM"
 
-rem napln pracovnˇ promennou mnou nebo Petulkou, pocita ze to je Peta - default, prepis na me pokud jsem to ja.
+rem napln pracovní promennou mnou nebo Petulkou, pocita ze to je Peta - default, prepis na me pokud jsem to ja.
 set primarniCil=%primarniCilPEM%
 if %USERNAME%==JIM set primarniCil=%primarniCilJIM%
+rem 2025-05-24 - doplnění synchro obsahu o miláčkův obsahu
+rem napln pracovní promennou mnou nebo Petulkou, pocita ze to je Peta - default, prepis na me pokud jsem to ja.
+set milackuvZdroj=%milackuvZdrojProPEM%
+if %USERNAME%==JIM set milackuvZdroj=%milackuvZdrojProJIM%
 
-rem otoźˇm zdroj a cˇl pro obnovu stavu pýedchozˇho - po reinstalacˇch
+rem otočím zdroj a cíl pro obnovu stavu předchozího - po reinstalacích
 set protismernyZdroj=%primarniCil%
 set protismernyCil=%primarniZdroj%
 
-rem ignorovan‚ files pro z lohy a obnovy
+rem ignorované files pro zálohy a obnovy
 set ignorujFiles=desktop.ini
 
-rem věkonn  ź st kopˇrov nˇ sem  a tam
+rem výkonná část kopírování sem  a tam
 echo =========================================================
 echo =========================================================
 echo =========================================================
 echo PRACUJI v kontextu %kdoJsem%
 echo
-echo Jsme u z lohy nověch a novŘjçˇch polo§ek do Google disk:
+echo Jsme u zálohy nových a novějších položek na Google disk:
 echo
-echo    1. ź st z MS3 do cloudu
-echo       a) kopˇruju z %primarniZdroj%
-echo       b) kopˇruju do %primarniCil%
-echo       c) vçe mimo %ignorujFiles%
-
+echo    1. část z MS3 do cloudu
+echo       a) kopíruju z %primarniZdroj%
+echo       b) kopíruju do %primarniCil%
+echo       c) vše mimo %ignorujFiles%
 PAUSE
 echo
-echo po klinutˇ se spustˇ nahr nˇ nověch a novŘjçˇch polo§ek do Google disku
+echo po klinutí se spustí nahrání nových a novějších položek do Google disku
 echo
 ROBOCOPY %primarniZdroj% %primarniCil% /E /XO /XF %ignorujFiles%
 echo
 echo
-echo 1. ź st je dokonźena
+echo 1. část je dokončena
 echo
 echo =========================================================
 echo =========================================================
 echo =========================================================
 echo
-echo Jsme u obnovy nověch a novŘjçˇch polo§ek na Google disku ke mŘ
+echo Jsme u obnovy nových a novějších položek z Google disku ke mě
 echo
-echo    2. ź st z cloudu do MS3
-echo       a) kopˇruju z %protismernyZdroj%
-echo       b) kopˇruju do %protismernyCil%
-echo       c) vçe mimo %ignorujFiles%
+echo    2.a. část z cloudu do MS3 (moje vlastní)
+echo       a) kopíruju z %protismernyZdroj%
+echo       b) kopíruju do %protismernyCil%
+echo       c) vše mimo %ignorujFiles%
 echo
-echo po klinutˇ se spustˇ obnova nověch a novŘjçˇch polo§ek z Google disku
+echo po klinutí se spustí obnova nových a novějších položek z Google disku (moje vlastní)
 echo
 PAUSE
 ROBOCOPY %protismernyZdroj% %protismernyCil% /E /XO /XF %ignorujFiles%
 echo
-echo 2. ź st je dokonźena, MS3 oçetýeno
-echo        PEM konźˇ
-echo        JIM pokraźuje ;o)
+PAUSE
+echo    2.b. část z cloudu do MS3 (mojeho miláčka)
+echo       a) kopíruju z %milackuvZdroj%
+echo       b) kopíruju do %protismernyCil%
+echo       c) vše mimo %ignorujFiles%
+echo
+echo po klinutí se spustí obnova nových a novějších položek z Google disku (mojeho miláčka)
+echo
+PAUSE
+rem echo /E :: copy subdirectories, including Empty ones.
+rem echo /XO :: eXclude Older files.
+rem echo /XF file [file]... :: eXclude Files matching given names/paths/wildcards.
+ROBOCOPY %milackuvZdroj% %protismernyCil% /E /XO /XF %ignorujFiles%
+echo
+PAUSE
+echo 2. část, a. i b., je dokončena, MS3 ošetřeno ;o)
+echo        PEM končí
+echo        JIM pokračuje ;o)
 echo
 echo =========================================================
 echo =========================================================
 echo =========================================================
 echo
-rem pokud nejsem JIM, pak sk źu na konec
+rem pokud nejsem JIM, pak skáču na konec
 if %kdoJsem%==PEM GOTO konecProNonJIMa
 
-rem jinak je jasně, §e jsem JIM, tak§e si jednou mŘsˇźnŘ odz lohuju PSTźka
-echo jen kdy§ jsem JIM a jen 1x v mŘsˇci provedu:
-echo zrcadlenˇ e-mailu - *.pst 1 x mŘsˇźnŘ (na zaź tku!!!)
-echo a odz lohovˇ adres ýa DATA na Fku
+rem jinak je jasný, že jsem JIM, takže si jednou měsíčně odzálohuju PSTčka
+echo jen když jsem JIM a jen 1x v měsíci provedu:
+echo zrcadlení e-mailu - *.pst 1 x měsíčně (na začátku!!!)
+echo a odzálohování adresáře DATA na Fku
 rem echo on
-echo Sestavenˇ kontrolnˇho souboru
+echo Sestavení kontrolního souboru
 PAUSE
-rem - nefunguje mi pou§itˇ t‚to promŘnn‚ d le :-/ set JIM_file = %date:~6,4%-%date:~3,2%.log
+rem - nefunguje mi použití této proměnné dále :-/ set JIM_file = %date:~6,4%-%date:~3,2%.log
 CD /D "D:\DATA\OneDrive\_Zalohy\emaily"
 IF EXIST %date:~6,4%-%date:~3,2%.log (
-	echo tento mŘsˇc ji§ bylo z lohov no - nez lohuju e-poźtu ni Fko
+	echo tento měsíc již bylo zálohováno - nezálohuju e-počtu ni Fko
 	) else (
 	type NUL > %date:~6,4%-%date:~3,2%.log
-	echo VYPNI si OUTLOOK !!!!, pak pokraźuj
+	echo VYPNI si OUTLOOK !!!!, pak pokračuj
 	PAUSE
 	ROBOCOPY "F:\DATA\email" "D:\DATA\OneDrive\_Zalohy\emaily" /Z /J
-	echo z loha *.pst dokonźena
-	rem doplnŘno 30.1.2024 pro z lohu VPC-źek
-	rem Łprava 23.3.2025 - po zmŘnŘ rozdŘlenˇ obsahu adres ýe "DATA" a reinstalaci JIMSNTB4 2024/2025 ruçˇm full F:\DATA\ z lohy, mimo e-mail…, viz věçe
-	rem echo JIMs z loha F:\DATA\ do PEMs OneDrive - D:\DATA\OneDrive\Data_Business_PEM\zalohy\zalohy_JIMs\ICT-F-DATA_all
+	echo záloha *.pst dokončena
+	rem doplněno 30.1.2024 pro zálohu VPC-ček
+	rem úprava 23.3.2025 - po změně rozdělení obsahu adresáře "DATA" a reinstalaci JIMSNTB4 2024/2025 ruším full F:\DATA\ zálohy, mimo e-mailů, viz výše
+	rem echo JIMs záloha F:\DATA\ do PEMs OneDrive - D:\DATA\OneDrive\Data_Business_PEM\zalohy\zalohy_JIMs\ICT-F-DATA_all
 	rem ROBOCOPY "F:\DATA\VPC" "D:\DATA\OneDrive\Data_Business_PEM\zalohy\zalohy_JIMs\ICT-F-DATA_all" /S /Z /J  /XD "F:\DATA\noCloud"
-	rem echo JIMs z loha F:\DATA dokoźena	
+	rem echo JIMs záloha F:\DATA dokočena	
 	)
 echo ***
 :konecProNonJIMa
@@ -124,6 +145,6 @@ echo ========================= KONEC =========================
 echo =========================================================
 echo =========================================================
 echo =========================================================
-echo ============== PO KLIKNUTÖ SE OKNO UZAVüE ===============
+echo ============== PO KLIKNUTÍ SE OKNO UZAVŘE ===============
 echo =========================================================
 PAUSE
